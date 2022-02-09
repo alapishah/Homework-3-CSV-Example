14c4369adaaf5c7723da21a92ffd258b0c855d38
Alapi <59810553+alapishah@users.noreply.github.com>
2022-02-09 11:07:50 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,61 @@
-var map = L.map('mapid').setView([37.7749, -122.4194], 13);
+    require([
+      "esri/Map",
+      "esri/layers/CSVLayer",
+      "esri/views/MapView",
+      "esri/config",
+      "esri/core/urlUtils",
+      "dojo/domReady!"
+    ], function(
+      Map,
+      CSVLayer,
+      MapView,
+      esriConfig,
+      urlUtils
+    ) {
 
- L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Terrain_Base/MapServer/tile/{z}/{y}/{x}', {
-	attribution: 'Tiles &copy; Esri &mdash; Source: USGS, Esri, TANA, DeLorme, and NPS',
-	maxZoom: 13
-}).addTo(map);
+      // If CSV files are not on the same domain as your website, a CORS enabled server
+      // or a proxy is required.
+     var url = "https://raw.githubusercontent.com/gbrunner/adv-programming-for-gis-and-rs/master/Web%20Development%20Module/Unit%202%20-%20ArcGIS%20JavaScript%20API/stl_crime_wgs_84.csv";
+     esriConfig.request.corsEnabledServers.push('https://rawgit.com');
 
-  // load GeoJSON from an external file
-  $.getJSON("https://raw.githubusercontent.com/gbrunner/adv-programming-for-gis-and-rs/master/Web%20Development%20Module/Unit%201%20-%20GitHub%20and%20Leaflet/sf_crime.geojson",function(data){
-         var ratIcon = L.icon({
-    iconUrl: 'https://e7.pngegg.com/pngimages/170/541/png-clipart-robbery-burglary-crime-theft-thief-mammal-people.png',
-    iconSize: [30,30]
-  }); 
-  L.geoJson(data  ,{
-    pointToLayer: function(feature,latlng){
-	    return L.marker(latlng,{icon: ratIcon});
-    },
-    onEachFeature: function (feature, layer) {
-      layer.bindPopup('<h1>Title: '+feature.properties.title+'</h1><p>Description:  '+feature.properties.description+'</p>');
-    }
-  }).addTo(map);
-});
+      // Paste the url into a browser's address bar to download and view the attributes
+      // in the CSV file. These attributes include:
+      // * mag - magnitude
+      // * type - earthquake or other event such as nuclear test
+      // * place - location of the event
+      // * time - the time of the event
 
+        const template = {
+          title: "Crime Info",
+          content: "Crimes Occuring in STL"
+        };
+
+        const csvLayer = new CSVLayer({
+          url: url,
+          copyright: "STL CRIME",
+          popupTemplate: template
+        });
+
+        var symbol = {
+          type: "simple-marker", 
+          color: "blue"
+        };
+
+      csvLayer.renderer = {
+        type: "simple", // autocasts as new SimpleRenderer()
+        symbol: symbol
+      };
+
+      var map = new Map({
+        basemap: "gray",
+        layers: [csvLayer]
+      });
+
+      var view = new MapView({
+        container: "viewDiv",
+        center:[-90.1994,38.6270],
+        zoom: 10,
+        map: map
+      });
+
+    });
